e7471397f4bbd990b433aee4a6dec40c86bd495e
sakuraprogramming <2118614145@qq.com>
2024-03-07 20:08:00 +0800

    第二次提交

---
+++ src/services/sakura-api-backend/userInterfaceInfoController.ts
@@ -0,0 +1,108 @@
+// @ts-ignore
+/* eslint-disable */
+import { request } from '@umijs/max';
+
+/** addUserInterfaceInfo POST /api/userInterfaceInfo/add */
+export async function addUserInterfaceInfoUsingPost(
+  body: API.UserInterfaceInfoAddRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseLong_>('/api/userInterfaceInfo/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** deleteUserInterfaceInfo POST /api/userInterfaceInfo/delete */
+export async function deleteUserInterfaceInfoUsingPost(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/userInterfaceInfo/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** getUserInterfaceInfoById GET /api/userInterfaceInfo/get */
+export async function getUserInterfaceInfoByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getUserInterfaceInfoByIdUsingGETParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseUserInterfaceInfo_>('/api/userInterfaceInfo/get', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** listUserInterface GET /api/userInterfaceInfo/list */
+export async function listUserInterfaceUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.listUserInterfaceUsingGETParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseListUserInterfaceInfo_>('/api/userInterfaceInfo/list', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** listUserInterfaceByPage GET /api/userInterfaceInfo/list/page */
+export async function listUserInterfaceByPageUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.listUserInterfaceByPageUsingGETParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponsePageUserInterfaceInfo_>('/api/userInterfaceInfo/list/page', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** openUserInterface POST /api/userInterfaceInfo/open */
+export async function openUserInterfaceUsingPost(
+  body: API.UserInterfaceInfoOpenRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/userInterfaceInfo/open', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** updateUserInterfaceInfo POST /api/userInterfaceInfo/update */
+export async function updateUserInterfaceInfoUsingPost(
+  body: API.UserInterfaceInfoUpdateRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/userInterfaceInfo/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
